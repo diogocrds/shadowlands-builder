a03acf96428ed1bbaa6c334bcec1d70d6c2b8f0c
Diogo J. Cardoso <diogoopequeno@hotmail.com>
2020-08-09 15:23:55 -0300

    Updates

---
+++ precache-manifest.3c8855e4a6184dad91981d41c979d9d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43dfabcbc490b8b46524f36a21ce7fb2",
+    "url": "/shadowlands-builder/index.html"
+  },
+  {
+    "revision": "616eb69f793fbdbe6852",
+    "url": "/shadowlands-builder/static/css/main.a1eae78e.chunk.css"
+  },
+  {
+    "revision": "5eff63bd8732435b7274",
+    "url": "/shadowlands-builder/static/js/2.af2dea59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shadowlands-builder/static/js/2.af2dea59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "616eb69f793fbdbe6852",
+    "url": "/shadowlands-builder/static/js/main.d7f6222c.chunk.js"
+  },
+  {
+    "revision": "b16e5b0d9e98c87203f1",
+    "url": "/shadowlands-builder/static/js/runtime-main.7120ecbd.js"
+  }
+]);
